99b248a68261200cbdca82b89cacd05a8118faa2
Carlinhos01 <carloseduardoce47437@gmail.com>
2024-05-13 22:32:01 -0300

    13/05/2024

--- pages/Home.js
+++ pages/Home.js
@@ -1,8 +1,9 @@
 import {useEffect, useState} from 'react';
-import {View, Text, Image, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, Text, Image, FlatList, TouchableOpacity, StyleSheet,ScrollView} from 'react-native';
 import {getDownloadURL, ref, uploadBytesResumable} from "firebase/storage";
 import {storage, fire} from "../Firebase";
 import { collection, onSnapshot } from 'firebase/firestore';
+import * as ImagePicker from "expo-image-picker";
 
 export default function Home(){
     const [img, setImg] = useState("");
@@ -48,34 +49,79 @@ export default function Home(){
         }
     }
 
+    async function pickImage(){
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+    
+    
+        if (!result.canceled) {
+          setImg(result.assets.uri);
+          await uploadImage(result.assets.uri, "image")
+        }
+      };
+
     return(
-        <View>
-            <Text>Minhas Fotos</Text>
+        
+        <View style={estilo.container}>
+            <Text style={estilo.titulo}>Minhas Fotos</Text>
             <FlatList
                 data={file}
                 keyExtractor={(item)=>item.url}
                 renderItem={(item)=>{
                     if(item.fileType ==="img"){
                         <Image
-                            source={{uri:item}}
+                            source={{uri:item}.url}
                             style={estilo.fotos}
                         />
                     }
                 }
                 }
+                numColumns={2}
             />
+
+                <TouchableOpacity 
+                    onPress={pickImage}
+                    style={estilo.imgpick}>
+                <Text style={estilo.subtitulo}>Images</Text>
+                </TouchableOpacity>
+      
         </View>
+        
     )
+    
 }
 
 const estilo = StyleSheet.create({
     container:{
     flex:1,
     justifyContent:'center',
-    alignItems:'center'
+    alignItems:'center',
+    
     },
     fotos:{
         width:200,
         height:200
+    },
+    titulo:{
+        fontSize: 35,
+        marginTop:100,
+    },
+    imgpick:{
+        position:"absolute",
+        justifyContent: 'center',
+        alignItems:'center',
+        textAlign:'center',
+        borderRadius:20,
+        backgroundColor: '#4e56a1',
+        width:150,
+        height:100,
+    },
+    subtitulo:{
+        fontSize:30,
     }
+    
 });
